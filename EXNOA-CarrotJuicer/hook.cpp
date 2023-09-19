fd24978b1455cf7fa4ae4493a31cfbae4a9ba618
KevinVG207 <kevin2000.van.geemen@gmail.com>
2023-09-19 23:07:23 +0200

    Export Localize.JP text

--- EXNOA-CarrotJuicer/hook.cpp
+++ EXNOA-CarrotJuicer/hook.cpp
@@ -6,6 +6,7 @@
 #include <Windows.h>
 #include <MinHook.h>
 
+#include "hook.hpp"
 #include "config.hpp"
 #include "edb.hpp"
 #include "responses.hpp"
@@ -16,6 +17,28 @@ using namespace std::literals;
 
 namespace
 {
+	void* find_nested_class_by_name(void* klass, const char* name)
+	{
+		printf("7\n");
+		il2cpp_class_get_nested_types_t il2cpp_class_get_nested_types = reinterpret_cast<il2cpp_class_get_nested_types_t>(GetProcAddress(GetModuleHandle(L"GameAssembly.dll"), "il2cpp_class_get_nested_types"));
+
+		void* iter{};
+		while (const auto curNestedClass = il2cpp_class_get_nested_types(klass, &iter))
+		{
+			printf("8\n");
+			if (static_cast<bool>(std::string_view(name) ==
+								static_cast<Il2CppClassHead*>(curNestedClass)->name))
+			{
+				printf("9\n");
+				return curNestedClass;
+			}
+		}
+		printf("10\n");
+		return nullptr;
+	}
+
+
+
 	void create_debug_console()
 	{
 		AllocConsole();
@@ -152,7 +175,68 @@ namespace
 		}
 		MH_CreateHook(LZ4_compress_default_ext_ptr, LZ4_compress_default_ext_hook, &LZ4_compress_default_ext_orig);
 		MH_EnableHook(LZ4_compress_default_ext_ptr);
+
+		// const auto umamusume_module = GetModuleHandle(L"umamusume.dll");
+		// const auto get_class = GetProcAddress
 	}
+
+	void* localize_jp_get_orig = nullptr;
+	Il2CppString* localize_jp_get_hook(int id)
+	{
+		printf("localize_jp_get_hook %d\n", id);
+
+		// Easiest way:
+		// Hash the jp text and use that as key.
+
+		// Find a way to loop over all text in the dictionary of Localize.JP OR the TextId enum.
+
+		// TODO: Try to access the dictionary of Localize.JP.
+
+
+
+
+		// Now I have the textID.
+		// Find what string this belongs to in the textid enum.
+		
+		// If dumping:
+		// Write as dictionary key (enum string) and value (jp text).
+
+		// If patching:
+		// Fetch translation from tl dict by enum string as key.
+
+		auto orig_result = reinterpret_cast<decltype(localize_jp_get_hook)*>(localize_jp_get_orig)(id);
+
+		return orig_result;
+	}
+
+	// Il2CppString* NewWStr(std::wstring_view str, HMODULE game_assembly_module)
+	// {
+	// 	il2cpp_string_new_utf16_t il2cpp_string_new_utf16 = reinterpret_cast<il2cpp_string_new_utf16_t>(GetProcAddress(game_assembly_module, "il2cpp_string_new_utf16"));
+	// 	return il2cpp_string_new_utf16(str.data(), str.size());
+	// }
+
+	// template <typename T = void*>
+	// T read_field(const void* ptr, const FieldInfo* field)
+	// {
+	// 	T result;
+	// 	const auto fieldPtr = static_cast<const std::byte*>(ptr) + field->offset;
+	// 	std::memcpy(std::addressof(result), fieldPtr, sizeof(T));
+	// 	return result;
+	// }
+
+	// template <typename T>
+	// T read_field(const void* ptr, TypedField<T> field)
+	// {
+	// 	return read_field<T>(ptr, field.Field);
+	// }
+
+	// template <typename T>
+	// void write_field(void* ptr, const FieldInfo* field, const T& value)
+	// {
+	// 	const auto fieldPtr = static_cast<std::byte*>(ptr) + field->offset;
+	// 	std::memcpy(fieldPtr, std::addressof(value), sizeof(T));
+	// }
+
 
 	void* load_library_w_orig = nullptr;
 
@@ -163,6 +247,89 @@ namespace
 		// GameAssembly.dll code must be loaded and decrypted while loading criware library
 		if (path == L"cri_ware_unity.dll"s)
 		{
+
+			const auto game_assembly_module = GetModuleHandle(L"GameAssembly.dll");
+
+
+			const il2cpp_domain_get_t il2cpp_domain_get = reinterpret_cast<il2cpp_domain_get_t>(GetProcAddress(game_assembly_module, "il2cpp_domain_get"));
+			const il2cpp_domain_assembly_open_t il2cpp_domain_assembly_open = reinterpret_cast<il2cpp_domain_assembly_open_t>(GetProcAddress(game_assembly_module, "il2cpp_domain_assembly_open"));
+			const il2cpp_assembly_get_image_t il2cpp_assembly_get_image = reinterpret_cast<il2cpp_assembly_get_image_t>(GetProcAddress(game_assembly_module, "il2cpp_assembly_get_image"));
+			const il2cpp_class_from_name_t il2cpp_class_from_name = reinterpret_cast<il2cpp_class_from_name_t>(GetProcAddress(game_assembly_module, "il2cpp_class_from_name"));
+			const il2cpp_class_get_method_from_name_t il2cpp_class_get_method_from_name = reinterpret_cast<il2cpp_class_get_method_from_name_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_method_from_name"));
+			const il2cpp_class_get_nested_types_t il2cpp_class_get_nested_types = reinterpret_cast<il2cpp_class_get_nested_types_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_nested_types"));
+			const il2cpp_class_get_field_from_name_t il2cpp_class_get_field_from_name = reinterpret_cast<il2cpp_class_get_field_from_name_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_field_from_name"));
+
+			printf("1\n");
+
+			auto domain = il2cpp_domain_get();
+			// Print domain
+			printf("Domain: %p\n", domain);
+
+
+			printf("1aaa\n");
+
+			auto assembly = il2cpp_domain_assembly_open(domain, "umamusume.dll");
+			// Print assembly
+			printf("Assembly: %p\n", assembly);
+
+			printf("2\n");
+			auto image = il2cpp_assembly_get_image(assembly);
+			// Print image
+			printf("Image: %p\n", image);
+
+			printf("3\n");
+			auto localize_class = il2cpp_class_from_name(image, "Gallop", "Localize");
+			printf("4\n");
+
+			const char* name = "JP";
+			const auto localize_jp_class = find_nested_class_by_name(localize_class, name);
+
+
+			// auto localize_jp_jpdict = il2cpp_class_get_field_from_name(localize_jp_class, "JPdict");
+
+			// auto jp_dict = read_field<Dictionary>(localize_jp_class, localize_jp_jpdict);
+
+
+			printf("5\n");
+
+			if (localize_jp_class == nullptr)
+			{
+				printf("Failed to find localize class\n");
+			}
+			else
+			{
+				auto localize_jp_get_addr = il2cpp_class_get_method_from_name(localize_jp_class, "Get", 1)->methodPointer;
+				printf("6\n");
+				printf("localize_jp_get_addr: %p\n", localize_jp_get_addr);
+
+
+				auto localize_jp_get_addr_offset = reinterpret_cast<void*>(localize_jp_get_addr);
+				printf("localize_jp_get_addr_offset: %p\n", localize_jp_get_addr_offset);
+
+				MH_CreateHook(localize_jp_get_addr_offset, localize_jp_get_hook, &localize_jp_get_orig);
+
+				// Extract the text
+				for (int i = 0; i <= 18446744073709551615; i++)
+				{
+					Il2CppString* jp_text = reinterpret_cast<decltype(localize_jp_get_hook)*>(localize_jp_get_orig)(i);
+
+					if (jp_text->length == 0)
+					{
+						continue;
+					}
+
+					printf("ID: %d, length: %d\n", i, jp_text->length);
+				}
+
+
+				MH_EnableHook(localize_jp_get_addr_offset);
+			}
+
+
+
+
+
+
 			bootstrap_carrot_juicer();
 
 			MH_DisableHook(LoadLibraryW);
