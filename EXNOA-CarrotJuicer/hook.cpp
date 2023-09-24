241846b7c360788f6bc77603a84eec2c78b7a31b
KevinVG207 <kevin2000.van.geemen@gmail.com>
2023-09-24 13:50:25 +0200

    Working text enum export

--- EXNOA-CarrotJuicer/hook.cpp
+++ EXNOA-CarrotJuicer/hook.cpp
@@ -1,10 +1,13 @@
 #include <filesystem>
 #include <iostream>
+#include <fstream>
 #include <locale>
 #include <string>
 #include <thread>
 #include <Windows.h>
 #include <MinHook.h>
+#include <map>
+#include <sstream>
 
 #include "hook.hpp"
 #include "config.hpp"
@@ -17,6 +20,107 @@ using namespace std::literals;
 
 namespace
 {
+	il2cpp_string_new_t il2cpp_string_new = nullptr;
+
+	bool tl_first_check = true;
+	std::filesystem::file_time_type tl_last_modified;
+	std::map<int, std::string> textid_to_text;
+
+
+	bool file_exists(std::string file_path)
+	{
+		std::ifstream infile(file_path);
+		bool file_exists = infile.good();
+		infile.close();
+		return file_exists;
+	}
+
+	// copy-pasted from https://stackoverflow.com/questions/3418231/replace-part-of-a-string-with-another-string
+	void replaceAll(std::string& str, const std::string& from, const std::string& to)
+	{
+		if (from.empty())
+			return;
+		size_t start_pos = 0;
+		while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+		{
+			str.replace(start_pos, from.length(), to);
+			start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+		}
+	}
+
+
+	void setup_tl_map(){
+		printf("Checking tl map\n");
+
+		std::string file_name = "translations.txt";
+
+		if (!file_exists(file_name)) {
+			return;
+		}
+
+		// Print tl_first_check
+		printf("tl_first_check: %d\n", tl_first_check);
+
+		if (tl_first_check)
+		{
+			tl_first_check = false;
+			tl_last_modified = std::filesystem::last_write_time(file_name);
+			printf("First check, tl_last_modified: %lld\n", tl_last_modified.time_since_epoch().count());
+		}
+		else
+		{
+			auto last_modified = std::filesystem::last_write_time(file_name);
+
+			printf("Previous tl_last_modified: %lld\n", tl_last_modified.time_since_epoch().count());
+			printf("Last modified: %lld\n", last_modified.time_since_epoch().count());
+
+			if (last_modified == tl_last_modified)
+			{
+				printf("No change in tl map\n");
+				return;
+			}
+			else
+			{
+				printf("Change in tl map\n");
+				tl_last_modified = last_modified;
+			}
+		}
+
+		printf("Setting up tl map\n");
+
+		std::ifstream infile(file_name);
+		std::string line;
+		while (std::getline(infile, line))
+		{
+			std::istringstream iss(line);
+			
+			// Split line by tab. First part is textid, second part is translation
+			std::string textid;
+			std::string translation;
+			std::getline(iss, textid, '\t');
+			std::getline(iss, translation, '\t');
+
+			// Skip if translation is empty
+			if (translation.empty() || translation.length() == 0)
+			{
+				continue;
+			}
+
+			// Convert textid to int
+			int textid_int = std::stoi(textid);
+
+			// Debug print
+			printf("textid: %d, translation: %s\n", textid_int, translation.c_str());
+
+			replaceAll(translation, "\\n", "\n");
+			replaceAll(translation, "\\r", "\r");
+			replaceAll(translation, "\\\"", "\"");
+
+			// Add to map
+			textid_to_text[textid_int] = translation;
+		}
+	}
+
 	void* find_nested_class_by_name(void* klass, const char* name)
 	{
 		printf("7\n");
@@ -37,7 +141,27 @@ namespace
 		return nullptr;
 	}
 
+	std::string il2cppstring_to_utf8(std::wstring str)
+	{
+		std::string result;
+		result.resize(str.length() * 4);
 
+		int len = WideCharToMultiByte(CP_UTF8, 0, str.data(), str.length(), result.data(), result.size(), nullptr, nullptr);
+
+		result.resize(len);
+
+		return result;
+	}
+
+	std::string il2cppstring_to_jsonstring(std::wstring str)
+	{
+		auto unicode = il2cppstring_to_utf8(str);
+		replaceAll(unicode, "\n", "\\n");
+		replaceAll(unicode, "\r", "\\r");
+		replaceAll(unicode, "\"", "\\\"");
+
+		return unicode;
+	}
 
 	void create_debug_console()
 	{
@@ -128,6 +252,9 @@ namespace
 		int srcSize,
 		int dstCapacity)
 	{
+		// Try refreshing the TL map
+		setup_tl_map();
+
 		const int ret = reinterpret_cast<decltype(LZ4_compress_default_ext_hook)*>(LZ4_compress_default_ext_orig)(
 			src, dst, srcSize, dstCapacity);
 
@@ -183,59 +310,201 @@ namespace
 	void* localize_jp_get_orig = nullptr;
 	Il2CppString* localize_jp_get_hook(int id)
 	{
-		printf("localize_jp_get_hook %d\n", id);
-
-		// Easiest way:
-		// Hash the jp text and use that as key.
-
-		// Find a way to loop over all text in the dictionary of Localize.JP OR the TextId enum.
-
-		// TODO: Try to access the dictionary of Localize.JP.
-
-
-
-
-		// Now I have the textID.
-		// Find what string this belongs to in the textid enum.
-		
-		// If dumping:
-		// Write as dictionary key (enum string) and value (jp text).
-
-		// If patching:
-		// Fetch translation from tl dict by enum string as key.
-
 		auto orig_result = reinterpret_cast<decltype(localize_jp_get_hook)*>(localize_jp_get_orig)(id);
+
+		if (id != 1030 && id != 1107)  // Don't print annoying time strings
+		{
+			std::string unicode = il2cppstring_to_jsonstring(orig_result->start_char);
+			printf("GET ID: %d: %s\n", id, unicode.c_str());
+		}
+
+		// If id is in tl dict, return translation
+		if (textid_to_text.find(id) != textid_to_text.end())
+		{
+			// Convert translation to wstring
+			std::string* translation_str = &textid_to_text[id];
+
+			return il2cpp_string_new(translation_str->data());
+		}
 
 		return orig_result;
 	}
 
-	// Il2CppString* NewWStr(std::wstring_view str, HMODULE game_assembly_module)
-	// {
-	// 	il2cpp_string_new_utf16_t il2cpp_string_new_utf16 = reinterpret_cast<il2cpp_string_new_utf16_t>(GetProcAddress(game_assembly_module, "il2cpp_string_new_utf16"));
-	// 	return il2cpp_string_new_utf16(str.data(), str.size());
-	// }
+	void* textcommon_setid_orig = nullptr;
+	void* textcommon_setid_hook (void* _this, int id)
+	{
+		printf("SETID: %d\n", id);
 
-	// template <typename T = void*>
-	// T read_field(const void* ptr, const FieldInfo* field)
-	// {
-	// 	T result;
-	// 	const auto fieldPtr = static_cast<const std::byte*>(ptr) + field->offset;
-	// 	std::memcpy(std::addressof(result), fieldPtr, sizeof(T));
-	// 	return result;
-	// }
+		return reinterpret_cast<decltype(textcommon_setid_hook)*>(textcommon_setid_orig) (
+			_this, id
+			);
+	}
 
-	// template <typename T>
-	// T read_field(const void* ptr, TypedField<T> field)
-	// {
-	// 	return read_field<T>(ptr, field.Field);
-	// }
+		void* textcommon_gettextidstring_orig = nullptr;
+	Il2CppString* textcommon_gettextidstring_hook (void* _this)
+	{
+		return reinterpret_cast<decltype(textcommon_gettextidstring_hook)*>(textcommon_gettextidstring_orig) (
+			_this
+			);
+	}
 
-	// template <typename T>
-	// void write_field(void* ptr, const FieldInfo* field, const T& value)
-	// {
-	// 	const auto fieldPtr = static_cast<std::byte*>(ptr) + field->offset;
-	// 	std::memcpy(fieldPtr, std::addressof(value), sizeof(T));
-	// }
+
+
+	void dump_text_db(void* textcommon_object)
+	{
+		// Dump DB to file
+		std::string file_name = "db_dump.json";
+		if (!file_exists(file_name))
+		{
+			printf("db_dump.json not found, skipping.\n");
+			return;
+		}
+
+		printf("Dumping DB to file\n");
+
+		std::ofstream outfile;
+		outfile.open(file_name, std::ios_base::trunc);
+
+		outfile << "{\n";
+
+		// Create an array to store the text
+		// Extract the text
+		bool first = true;
+		for (int i = 0; i <= 99999; i++)
+		{
+			textcommon_setid_hook(textcommon_object, i);
+			Il2CppString* text_id_string = textcommon_gettextidstring_hook(textcommon_object);
+			Il2CppString* jp_text = reinterpret_cast<decltype(localize_jp_get_hook)*>(localize_jp_get_orig)(i);
+
+			if (jp_text->length == 0)
+			{
+				continue;
+			}
+
+			if (!first)
+			{
+				outfile << ",\n";
+			}
+			else
+			{
+				first = false;
+			}
+
+			// printf("ID: %d, length: %d\n", i, jp_text->length);
+
+			std::string unicode = il2cppstring_to_jsonstring(text_id_string->start_char);
+			std::string unicode2 = il2cppstring_to_jsonstring(jp_text->start_char);
+
+			// // Convert i to string
+			// char buffer [50];
+			// sprintf_s(buffer, "%d", i);
+			// std::string textid = buffer;
+
+			outfile << "\t\"" << unicode << "\": \"" << unicode2 << "\"";
+
+		}
+
+		outfile << "\n}";
+
+		outfile.close();
+	}
+
+
+	void* populate_with_errors_orig = nullptr;
+	bool populate_with_errors_hook(void* _this, Il2CppString* str, TextGenerationSettings_t* settings, void* context)
+	{
+		// printf("populate_with_errors_hook: %ls\n", str->start_char);
+		// printf("%ls\n\n", environment_get_stacktrace()->start_char);
+
+		std::string unicode = il2cppstring_to_utf8(str->start_char);
+
+		std::string unicode2 = il2cppstring_to_jsonstring(str->start_char);
+		printf("TEXT: %s\n", unicode2.c_str());
+
+		// if string starts with <nb>
+		if (unicode.find("<nb>") != std::string::npos)
+		{
+			replaceAll(unicode, "<nb>", "");
+			settings->horizontalOverflow = 1;
+		}
+		if (unicode.find("<slogan>") != std::string::npos)
+		{
+			replaceAll(unicode, "<slogan>", "");
+			replaceAll(unicode, "\n", "");
+			settings->horizontalOverflow = 0;
+		}
+
+		Il2CppString* new_str = il2cpp_string_new(unicode.c_str());
+
+		return reinterpret_cast<decltype(populate_with_errors_hook)*>(populate_with_errors_orig) (
+			_this, new_str, settings, context
+			);
+	}
+
+	void* localize_extension_text_orig = nullptr;
+	Il2CppString* localize_extension_text_hook(int id, int region)
+	{
+		auto orig_result = reinterpret_cast<decltype(localize_extension_text_hook)*>(localize_extension_text_orig)(id, region);
+
+		std::string unicode = il2cppstring_to_jsonstring(orig_result->start_char);
+		printf("EXTENSION ID: %d: %s\n", id, unicode.c_str());
+
+		return orig_result;
+	}
+
+	void* localize_get_orig = nullptr;
+	Il2CppString* localize_get_hook(int id)
+	{
+		auto orig_result = reinterpret_cast<decltype(localize_get_hook)*>(localize_get_orig)(id);
+
+		std::string unicode = il2cppstring_to_jsonstring(orig_result->start_char);
+		printf("GET2 ID: %d: %s\n", id, unicode.c_str());
+
+		return orig_result;
+	}
+
+
+	void* textcommon_gettextid_orig = nullptr;
+	int textcommon_gettextid_hook (void* _this)
+	{
+		return reinterpret_cast<decltype(textcommon_gettextid_hook)*>(textcommon_gettextid_orig) (
+			_this
+			);
+		
+		// printf("GETTEXTID: %d\n", orig_result);
+
+		// return orig_result;
+	}
+
+	bool first_textcommon = true;
+
+	void* textcommon_settext_orig = nullptr;
+	void* textcommon_settext_hook (void* _this, Il2CppString* str)
+	{
+		int textid = reinterpret_cast<decltype(textcommon_gettextid_hook)*>(textcommon_gettextid_orig) (
+			_this
+			);
+
+		std::string id_string = il2cppstring_to_jsonstring(textcommon_gettextidstring_hook(_this)->start_char);
+
+		std::string unicode = il2cppstring_to_jsonstring(str->start_char);
+
+		printf("===SETTEXT===\n");
+		printf("ID: %d\n", textid);
+		printf("ID STRING: %s\n", id_string.c_str());
+		printf("TEXT: %s\n", unicode.c_str());
+
+		if (first_textcommon)
+		{
+			first_textcommon = false;
+			dump_text_db(_this);
+			textcommon_setid_hook(_this, textid);
+		}
+
+		return reinterpret_cast<decltype(textcommon_settext_hook)*>(textcommon_settext_orig) (
+			_this, str
+			);
+	}
 
 
 	void* load_library_w_orig = nullptr;
@@ -247,7 +516,6 @@ namespace
 		// GameAssembly.dll code must be loaded and decrypted while loading criware library
 		if (path == L"cri_ware_unity.dll"s)
 		{
-
 			const auto game_assembly_module = GetModuleHandle(L"GameAssembly.dll");
 
 
@@ -258,12 +526,41 @@ namespace
 			const il2cpp_class_get_method_from_name_t il2cpp_class_get_method_from_name = reinterpret_cast<il2cpp_class_get_method_from_name_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_method_from_name"));
 			const il2cpp_class_get_nested_types_t il2cpp_class_get_nested_types = reinterpret_cast<il2cpp_class_get_nested_types_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_nested_types"));
 			const il2cpp_class_get_field_from_name_t il2cpp_class_get_field_from_name = reinterpret_cast<il2cpp_class_get_field_from_name_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_field_from_name"));
+			const il2cpp_class_enum_basetype_t il2cpp_class_enum_basetype = reinterpret_cast<il2cpp_class_enum_basetype_t>(GetProcAddress(game_assembly_module, "il2cpp_class_enum_basetype"));
+			const il2cpp_class_get_methods_t il2cpp_class_get_methods = reinterpret_cast<il2cpp_class_get_methods_t>(GetProcAddress(game_assembly_module, "il2cpp_class_get_methods"));
+			il2cpp_string_new = reinterpret_cast<il2cpp_string_new_t>(GetProcAddress(game_assembly_module, "il2cpp_string_new"));
 
 			printf("1\n");
 
 			auto domain = il2cpp_domain_get();
 			// Print domain
 			printf("Domain: %p\n", domain);
+
+
+
+
+
+			auto assembly2 = il2cpp_domain_assembly_open(domain, "UnityEngine.TextRenderingModule.dll");
+			printf("Assembly2: %p\n", assembly2);
+			auto image2 = il2cpp_assembly_get_image(assembly2);
+			printf("Image2: %p\n", image2);
+			auto text_generator_class = il2cpp_class_from_name(image2, "UnityEngine", "TextGenerator");
+			printf("TextGenerator: %p\n", text_generator_class);
+			auto populate_with_errors_addr = il2cpp_class_get_method_from_name(text_generator_class, "PopulateWithErrors", 3)->methodPointer;
+			printf("populate_with_errors_addr: %p\n", populate_with_errors_addr);
+			auto populate_with_errors_addr_offset = reinterpret_cast<void*>(populate_with_errors_addr);
+
+			MH_CreateHook(populate_with_errors_addr_offset, populate_with_errors_hook, &populate_with_errors_orig);
+			MH_EnableHook(populate_with_errors_addr_offset);
+
+
+
+			// auto populate_with_errors_addr = get_method_pointer(
+			// "UnityEngine.TextRenderingModule.dll",
+			// "UnityEngine", "TextGenerator",
+			// "PopulateWithErrors", 3
+			// )
+
 
 
 			printf("1aaa\n");
@@ -277,12 +574,143 @@ namespace
 			// Print image
 			printf("Image: %p\n", image);
 
+
+			// const auto system_runtime_module = GetModuleHandle(L"System.Runtime.dll");
+			// printf("System.Runtime.dll at %p\n", system_runtime_module);
+
+			// const auto enum_class = il2cpp_class_from_name(system_runtime_module, "System", "Enum");
+			// printf("enum_class: %p\n", enum_class);
+
+			// const auto enum_getnames_addr = il2cpp_class_get_method_from_name(enum_class, "GetNames", 1)->methodPointer;
+			// printf("enum_getnames_addr: %p\n", enum_getnames_addr);
+
+			// while (true)
+			// {
+			// 	continue;
+			// }
+
+
+			// // Get TextId enum that is umamusume.dll Gallop TextId
+			// auto textid_enum_class = il2cpp_class_from_name(image, "Gallop", "TextId");
+			// printf("textid_enum: %p\n", textid_enum_class);
+
+			// auto textid_enum_basetype = il2cpp_class_enum_basetype(textid_enum_class);
+			// printf("textid_enum_basetype: %p\n", textid_enum_basetype);
+
+
+			// void* iter = nullptr;
+			// printf("f");
+			// while (const MethodInfo* method = il2cpp_class_get_methods(textid_enum_class, &iter))
+			// {
+			// 	printf("method: %p\n", method->name);
+			// }
+
+			// printf("g");
+
+
+			// while (true)
+			// {
+			// 	continue;
+			// }
+
+
+			// auto localize_extension_class = il2cpp_class_from_name(image, "Gallop", "LocalizeExtention");
+			// printf("localize_extension_class: %p\n", localize_extension_class);
+
+			// auto localize_extension_text_addr = il2cpp_class_get_method_from_name(localize_extension_class, "GetInsertHalfSizeSpace", 2)->methodPointer;
+			// printf("localize_extension_text_addr: %p\n", localize_extension_text_addr);
+
+			// auto localize_extension_text_addr_offset = reinterpret_cast<void*>(localize_extension_text_addr);
+			// printf("localize_extension_text_addr_offset: %p\n", localize_extension_text_addr_offset);
+
+			// MH_CreateHook(localize_extension_text_addr_offset, localize_extension_text_hook, &localize_extension_text_orig);
+			// MH_EnableHook(localize_extension_text_addr_offset);
+
+
+			auto textcommon_class = il2cpp_class_from_name(image, "Gallop", "TextCommon");
+			printf("textcommon_class: %p\n", textcommon_class);
+
+
+
+
+			auto textcommon_setid_addr = il2cpp_class_get_method_from_name(textcommon_class, "set_TextId", 1)->methodPointer;
+			printf("textcommon_setid_addr: %p\n", textcommon_setid_addr);
+
+			auto textcommon_setid_addr_offset = reinterpret_cast<void*>(textcommon_setid_addr);
+			printf("textcommon_setid_addr_offset: %p\n", textcommon_setid_addr_offset);
+
+			MH_CreateHook(textcommon_setid_addr_offset, textcommon_setid_hook, &textcommon_setid_orig);
+			MH_EnableHook(textcommon_setid_addr_offset);
+
+
+			auto textcommon_gettextid_addr = il2cpp_class_get_method_from_name(textcommon_class, "get_TextId", 0)->methodPointer;
+			printf("textcommon_gettextid_addr: %p\n", textcommon_gettextid_addr);
+
+			auto textcommon_gettextid_addr_offset = reinterpret_cast<void*>(textcommon_gettextid_addr);
+			printf("textcommon_gettextid_addr_offset: %p\n", textcommon_gettextid_addr_offset);
+
+			MH_CreateHook(textcommon_gettextid_addr_offset, textcommon_gettextid_hook, &textcommon_gettextid_orig);
+			MH_EnableHook(textcommon_gettextid_addr_offset);
+
+
+
+			auto textcommon_gettextidstring_addr = il2cpp_class_get_method_from_name(textcommon_class, "get_TextIdString", 0)->methodPointer;
+			printf("textcommon_gettextidstring_addr: %p\n", textcommon_gettextidstring_addr);
+
+			auto textcommon_gettextidstring_addr_offset = reinterpret_cast<void*>(textcommon_gettextidstring_addr);
+			printf("textcommon_gettextidstring_addr_offset: %p\n", textcommon_gettextidstring_addr_offset);
+
+			MH_CreateHook(textcommon_gettextidstring_addr_offset, textcommon_gettextidstring_hook, &textcommon_gettextidstring_orig);
+
+
+
+
+
+			auto textcommon_settext_addr = il2cpp_class_get_method_from_name(textcommon_class, "set_text", 1)->methodPointer;
+			printf("textcommon_settext_addr: %p\n", textcommon_settext_addr);
+
+			auto textcommon_settext_addr_offset = reinterpret_cast<void*>(textcommon_settext_addr);
+			printf("textcommon_settext_addr_offset: %p\n", textcommon_settext_addr_offset);
+
+			MH_CreateHook(textcommon_settext_addr_offset, textcommon_settext_hook, &textcommon_settext_orig);
+			MH_EnableHook(textcommon_settext_addr_offset);
+
+
+
+
+
+
+
+
 			printf("3\n");
 			auto localize_class = il2cpp_class_from_name(image, "Gallop", "Localize");
 			printf("4\n");
 
+
+
+
+
+			// auto localize_get_addr = il2cpp_class_get_method_from_name(localize_class, "Get", 1)->methodPointer;
+			// printf("localize_get_addr: %p\n", localize_get_addr);
+
+			// auto localize_get_addr_offset = reinterpret_cast<void*>(localize_get_addr);
+			// printf("localize_get_addr_offset: %p\n", localize_get_addr_offset);
+
+			// MH_CreateHook(localize_get_addr_offset, localize_get_hook, &localize_get_orig);
+			// MH_EnableHook(localize_get_addr_offset);
+
+
+
+
+
+
+
+
+
+
 			const char* name = "JP";
 			const auto localize_jp_class = find_nested_class_by_name(localize_class, name);
+			printf("localize_jp_class: %p\n", localize_jp_class);
 
 
 			// auto localize_jp_jpdict = il2cpp_class_get_field_from_name(localize_jp_class, "JPdict");
@@ -308,25 +736,17 @@ namespace
 
 				MH_CreateHook(localize_jp_get_addr_offset, localize_jp_get_hook, &localize_jp_get_orig);
 
-				// Extract the text
-				for (int i = 0; i <= 99999; i++)
-				{
-					Il2CppString* jp_text = reinterpret_cast<decltype(localize_jp_get_hook)*>(localize_jp_get_orig)(i);
+				std::string file_name = "db_dump.json";
 
-					if (jp_text->length == 0)
-					{
-						continue;
-					}
+				printf("7\n");
 
-					printf("ID: %d, length: %d\n", i, jp_text->length);
-				}
-
+				// Check if translations file exists
+				setup_tl_map();
 
 				MH_EnableHook(localize_jp_get_addr_offset);
+
+				printf("Done\n");
 			}
-
-
-
 
 
 
--- EXNOA-CarrotJuicer/hook.hpp
+++ EXNOA-CarrotJuicer/hook.hpp
@@ -54,6 +54,8 @@ typedef struct Il2CppType
 	unsigned int pinned : 1;
 } Il2CppType;
 
+typedef struct Il2CppClass;
+
 // not real Il2CppString class
 struct Il2CppString
 {
@@ -61,6 +63,72 @@ struct Il2CppString
 	void* WhiteChars;
 	int32_t length;
 	wchar_t start_char[1];
+};
+
+// UnityEngine.Color
+struct Color_t
+{
+public:
+	// System.Single UnityEngine.Color::r
+	float r;
+	// System.Single UnityEngine.Color::g
+	float g;
+	// System.Single UnityEngine.Color::b
+	float b;
+	// System.Single UnityEngine.Color::a
+	float a;
+};
+
+// UnityEngine.Vector2
+struct Vector2_t
+{
+public:
+	// System.Single UnityEngine.Vector2::x
+	float x;
+	// System.Single UnityEngine.Vector2::y
+	float y;
+};
+
+// UnityEngine.TextGenerationSettings
+struct TextGenerationSettings_t
+{
+public:
+	// UnityEngine.Font UnityEngine.TextGenerationSettings::font
+	void* font;
+	// UnityEngine.Color UnityEngine.TextGenerationSettings::color
+	Color_t color;
+	// System.Int32 UnityEngine.TextGenerationSettings::fontSize
+	int32_t fontSize;
+	// System.Single UnityEngine.TextGenerationSettings::lineSpacing
+	float lineSpacing;
+	// System.Boolean UnityEngine.TextGenerationSettings::richText
+	bool richText;
+	// System.Single UnityEngine.TextGenerationSettings::scaleFactor
+	float scaleFactor;
+	// UnityEngine.FontStyle UnityEngine.TextGenerationSettings::fontStyle
+	int32_t fontStyle;
+	// UnityEngine.TextAnchor UnityEngine.TextGenerationSettings::textAnchor
+	int32_t textAnchor;
+	// System.Boolean UnityEngine.TextGenerationSettings::alignByGeometry
+	bool alignByGeometry;
+	// System.Boolean UnityEngine.TextGenerationSettings::resizeTextForBestFit
+	bool resizeTextForBestFit;
+	// System.Int32 UnityEngine.TextGenerationSettings::resizeTextMinSize
+	int32_t resizeTextMinSize;
+	// System.Int32 UnityEngine.TextGenerationSettings::resizeTextMaxSize
+	int32_t resizeTextMaxSize;
+	// System.Boolean UnityEngine.TextGenerationSettings::updateBounds
+	bool updateBounds;
+	// UnityEngine.VerticalWrapMode UnityEngine.TextGenerationSettings::verticalOverflow
+	int32_t verticalOverflow;
+	// UnityEngine.HorizontalWrapMode UnityEngine.TextGenerationSettings::horizontalOverflow
+	int32_t horizontalOverflow;
+	// UnityEngine.Vector2 UnityEngine.TextGenerationSettings::generationExtents
+	Vector2_t  generationExtents;
+	// UnityEngine.Vector2 UnityEngine.TextGenerationSettings::pivot
+	Vector2_t  pivot;
+	// System.Boolean UnityEngine.TextGenerationSettings::generateOutOfBounds
+	bool generateOutOfBounds;
 };
 
 struct ParameterInfo
@@ -163,6 +231,7 @@ typedef void* (*il2cpp_gchandle_get_target_t)(uint32_t gchandle);
 typedef void* (*il2cpp_class_from_type_t)(const Il2CppType* type);
 typedef void (*il2cpp_runtime_class_init_t)(void* klass);
 typedef void* (*il2cpp_runtime_invoke_t)(MethodInfo* method, void* obj, void** params, Il2CppObject** exc);
+typedef Il2CppType* (*il2cpp_class_enum_basetype_t)(void * klass);
 
 // function defines
 extern il2cpp_string_new_utf16_t il2cpp_string_new_utf16;
@@ -191,3 +260,4 @@ extern il2cpp_gchandle_get_target_t il2cpp_gchandle_get_target;
 extern il2cpp_class_from_type_t il2cpp_class_from_type;
 extern il2cpp_runtime_class_init_t il2cpp_runtime_class_init;
 extern il2cpp_runtime_invoke_t il2cpp_runtime_invoke;
+extern il2cpp_class_enum_basetype_t il2cpp_class_enum_basetype;
